2641c0ae65621e7bc635156d54de13ae048afe28
Felipe N. Schuch <felipe.schuch@acad.pucrs.br>
2020-08-20 13:20:07 -0300

    updating setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 import setuptools
+import versioneer
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="xcompact3d_toolbox",
-    version="0.1.3",
+    version=versioneer.get_version(),
     author="Felipe N. Schuch",
     author_email="felipe.schuch@edu.pucrs.br",
     description="A set of tools for pre and postprocessing prepared for the high-order Navier-Stokes solver Xcompact3d",
