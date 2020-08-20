9fde5338984dc3787a0a1183654872ab8a1240b7
Felipe N. Schuch <felipe.schuch@acad.pucrs.br>
2020-08-20 13:19:41 -0300

    Adding versioneer support for releasing

--- xcompact3d_toolbox/__init__.py
+++ xcompact3d_toolbox/__init__.py
@@ -3,3 +3,7 @@ from .param import mytype
 from .parameters import Parameters
 from .genepsi import gene_epsi_3D
 from .io import readfield, read_all, write_xdmf
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
