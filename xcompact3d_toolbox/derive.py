fix: sandbox with single precision

xcompact3d_toolbox/derive.py
@@ -1,8 +1,8 @@
-from .param import mytype
+from .param import param
 import scipy.sparse as sp
 
 
-def SecondDerivative(n, d, ncl1=2, ncln=2, npaire=1):
+def SecondDerivative(n, d=None, ncl1=2, ncln=2, npaire=1, coord=None):
     """
     f_xx = (-1*f[i-2]+16*f[i-1]-30*f[i+0]+16*f[i+1]-1*f[i+2])/(12*h**2)
     """
@@ -10,7 +10,7 @@ def SecondDerivative(n, d, ncl1=2, ncln=2, npaire=1):
         [-1.0, 16.0, -30.0, 16.0, -1.0],
         offsets=[-2, -1, 0, 1, 2],
         shape=(n, n),
-        dtype=mytype,
+        dtype=param["mytype"],
         format="lil",
     )
 
@@ -97,7 +97,7 @@ def FirstDerivative(n, d, ncl1=2, ncln=2, npaire=1):
         [1.0, -8.0, 8.0, -1],
         offsets=[-2, -1, 1, 2],
         shape=(n, n),
-        dtype=mytype,
+        dtype=param["mytype"],
         format="lil",
     )
 

xcompact3d_toolbox/mesh.py
@@ -1,4 +1,4 @@
-from .param import mytype
+from .param import param
 import numpy as np
 
 
@@ -19,7 +19,10 @@ def get_mesh(prm, raf=False, staggered=False):
             n[d] = int(m[d] * prm.nraf + 1)
     # Mesh
     mesh = {
-        d: np.linspace(start=0.0, stop=l[d], num=n[d], endpoint=not ncl[d]) for d in dim
+        d: np.linspace(
+            start=0.0, stop=l[d], num=n[d], endpoint=not ncl[d], dtype=param["mytype"]
+        )
+        for d in dim
     }
     #
     # Half-staggered for pressure
@@ -32,6 +35,7 @@ def get_mesh(prm, raf=False, staggered=False):
                 stop=l[d] - 0.5 * delta,
                 num=m[d],
                 endpoint=not ncl[d],
+                dtype=param["mytype"],
             )
     #
     # stretching

xcompact3d_toolbox/param.py
@@ -1,15 +1,6 @@
 from numpy import float64
 
-mytype = float64
-""":obj:`numpy.float32` or :obj:`numpy.float64`: Defines the float point precision
-
-This precision is applyed for I/O and for initialization at
-:obj:`xcompact3d_toolbox.sandbox`.
-
-Use :obj:`numpy.float64` (default) if Xcompact3d was compiled with the flag
-``-DDOUBLE_PREC``, or use ``np.float32`` otherwise (including ``-DSAVE_SINGLE``).
-
-"""
+param = {"mytype": float64}
 
 
 def boundary_condition(prm, var=None):