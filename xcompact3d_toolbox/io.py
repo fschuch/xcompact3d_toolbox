fix: sandbox with single precision

xcompact3d_toolbox/io.py
@@ -25,7 +25,7 @@ Notes
 
 """
 
-from .param import mytype, boundary_condition
+from .param import param, boundary_condition
 import numpy as np
 import xarray as xr
 import os.path
@@ -47,7 +47,7 @@ def readfield(filename, prm, dims="auto", coords=None, name=None, attrs=None):
 
     Attributes include the proper boundary conditions for derivatives if the
     file prefix is ``ux``, ``uy``, ``uz``, ``phi`` or ``pp``. Data type is
-    defined by :obj:`xcompact3d_toolbox.mytype`.
+    defined by :obj:`xcompact3d_toolbox.param["mytype"]`.
 
     Parameters
     ----------
@@ -81,8 +81,8 @@ def readfield(filename, prm, dims="auto", coords=None, name=None, attrs=None):
 
     >>> prm = x3d.Parameters()
 
-    >>> xcompact3d_toolbox.mytype = np.float64 # if x3d was compiled with `-DDOUBLE_PREC`
-    >>> xcompact3d_toolbox.mytype = np.float32 # otherwise
+    >>> xcompact3d_toolbox.param["mytype"] = np.float64 # if x3d was compiled with `-DDOUBLE_PREC`
+    >>> xcompact3d_toolbox.param["mytype"] = np.float32 # otherwise
 
     In the following cases, coordinates and dimensions are infered from the
     folder containing the file:
@@ -139,8 +139,13 @@ def readfield(filename, prm, dims="auto", coords=None, name=None, attrs=None):
         for key, value in coords.items():
             shape.append(value.size)
 
+    if os.path.islink(filename):
+        from os import readlink
+
+        filename = readlink(filename)
+
     return xr.DataArray(
-        np.fromfile(filename, dtype=mytype).reshape(shape, order="F"),
+        np.fromfile(filename, dtype=param["mytype"]).reshape(shape, order="F"),
         dims=dims,
         coords=coords,
         name=name,
@@ -176,8 +181,8 @@ def read_all(filename_pattern, prm):
 
     >>> prm = x3d.Parameters()
 
-    >>> x3d.mytype = np.float64 # if x3d was compiled with `-DDOUBLE_PREC`
-    >>> x3d.mytype = np.float32 # otherwise
+    >>> x3d.param["mytype"] = np.float64 # if x3d was compiled with `-DDOUBLE_PREC`
+    >>> x3d.param["mytype"] = np.float32 # otherwise
 
     In the following cases, coordinates and dimensions are infered from the
     folder containing the file and time from the filenames:
@@ -193,10 +198,10 @@ def read_all(filename_pattern, prm):
     dt = prm.dt
     t = dt * np.array(
         [
-            mytype(os.path.basename(file).split("-")[-1].split(".")[0])
+            param["mytype"](os.path.basename(file).split("-")[-1].split(".")[0])
             for file in filenames
         ],
-        dtype=mytype,
+        dtype=param["mytype"],
     )
 
     # numscalar = prm.dict['BasicParam'].get('numscalar', 0)
@@ -280,7 +285,7 @@ def write_xdmf(prm):
         elif folder == "yz_planes":
             nx, dx = 0, 0
 
-        prec = 8 if mytype == np.float64 else 4
+        prec = 8 if param["mytype"] == np.float64 else 4
 
         ibm_flag = "ibm" in prefixes
 

xcompact3d_toolbox/parameters.py
@@ -6,7 +6,7 @@ import numpy as np
 import math
 import traitlets
 import ipywidgets as widgets
-from .param import mytype
+from .param import param
 from .mesh import get_mesh
 from .io import i3d_to_dict, dict_to_i3d, write_xdmf
 
@@ -1295,7 +1295,7 @@ class Parameters(traitlets.HasTraits):
                     return "%3.1f %s" % (num, x)
                 num /= step_unit
 
-        prec = 4 if mytype == np.float32 else 8
+        prec = 4 if param["mytype"] == np.float32 else 8
 
         # Restart Size from tools.f90
         count = 3 + self.numscalar  # ux, uy, uz, phi

xcompact3d_toolbox/sandbox.py
@@ -12,7 +12,7 @@ See https://github.com/fschuch/Xcompact3d/ for more information.
 
 from .array import X3dDataArray, X3dDataset
 from .mesh import get_mesh
-from .param import mytype
+from .param import param
 import numpy as np
 import os.path
 import xarray as xr
@@ -123,7 +123,7 @@ def init_dataset(prm):
     -------
     :obj:`xarray.Dataset`
         Each variable is initialized with
-        ``np.zeros(dtype=xcompact3d_toolbox.mytype)`` and wrapped into a
+        ``np.zeros(dtype=xcompact3d_toolbox.param["mytype"])`` and wrapped into a
         :obj:`xarray.Dataset` with the proper size, dimensions, coordinates and
         attributes, check them for more details. The variables are:
 
@@ -174,7 +174,7 @@ def init_dataset(prm):
     if prm.nclx1 == 2:
         for i, var in enumerate("bxx1 bxy1 bxz1 noise_mod_x1".split()):
             ds[var] = xr.DataArray(
-                mytype(0.0),
+                param["mytype"](0.0),
                 dims=["y", "z"],
                 coords=[ds.y, ds.z],
                 attrs={
@@ -191,7 +191,7 @@ def init_dataset(prm):
     if prm.numscalar != 0:
         if prm.nclxS1 == 2:
             ds["bxphi1"] = xr.DataArray(
-                mytype(0.0),
+                param["mytype"](0.0),
                 dims=["n", "y", "z"],
                 coords=[ds.n, ds.y, ds.z],
                 attrs={
@@ -202,7 +202,7 @@ def init_dataset(prm):
             )
         if prm.nclyS1 == 2:
             ds["byphi1"] = xr.DataArray(
-                mytype(0.0),
+                param["mytype"](0.0),
                 dims=["n", "x", "z"],
                 coords=[ds.n, ds.x, ds.z],
                 attrs={
@@ -213,7 +213,7 @@ def init_dataset(prm):
             )
         if prm.nclySn == 2:
             ds["byphin"] = xr.DataArray(
-                mytype(0.0),
+                param["mytype"](0.0),
                 dims=["n", "x", "z"],
                 coords=[ds.n, ds.x, ds.z],
                 attrs={
@@ -225,7 +225,7 @@ def init_dataset(prm):
     # Initial Condition
     for i, var in enumerate(["ux", "uy", "uz"]):
         ds[var] = xr.DataArray(
-            mytype(0.0),
+            param["mytype"](0.0),
             dims=["x", "y", "z"],
             coords=[ds.x, ds.y, ds.z],
             attrs={
@@ -236,7 +236,7 @@ def init_dataset(prm):
         )
     if prm.numscalar != 0:
         ds["phi"] = xr.DataArray(
-            mytype(0.0),
+            param["mytype"](0.0),
             dims=["n", "x", "y", "z"],
             coords=[ds.n, ds.x, ds.y, ds.z],
             attrs={
@@ -248,7 +248,7 @@ def init_dataset(prm):
     # Flowrate control
     if prm.nclx1 == 0 and prm.nclxn == 0:
         ds["vol_frc"] = xr.DataArray(
-            mytype(0.0),
+            param["mytype"](0.0),
             dims=["x", "y", "z"],
             coords=[ds.x, ds.y, ds.z],
             attrs={